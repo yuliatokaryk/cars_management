350ca009985cebaaca47c71037d405186c2d44c3
yuliatokaryk <julia4099@gmail.com>
2022-10-31 12:54:17 +0200

    task 2.2

---
+++ database.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
+class Database
+  attr_accessor :database_file
+
+  def initialize(database_name)
+    @database_file = "#{database_name}.yml"
+    return fetch if File.exists?(database_file)
+    File.new(database_file, 'w+')
+  end
+
+  def record(modifyded_data)
+    File.open(database_file, 'r+') { |file| file.write(modifyded_data.to_yaml) }
+  end
+
+  def fetch
+    YAML.load_file(database_file) || []
+  end
+end
