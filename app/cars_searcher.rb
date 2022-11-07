50c3eb807e060b6c79a24b089edbbba36a72645c
yuliatokaryk <julia4099@gmail.com>
2022-11-07 09:19:07 +0200

    bugs fixed

--- app/cars_searcher.rb
+++ app/cars_searcher.rb
@@ -6,7 +6,6 @@ class CarsSearcher
   def initialize(db, rules)
     @cars_list = db.fetch
     @search_rules = rules
-    @total_quantity = cars_list.length
   end
 
   def call
@@ -14,25 +13,34 @@ class CarsSearcher
     general_choosing('model')
     range_determining('year_from', 'year_to')
     range_determining('price_from', 'price_to')
+    count_total_quantity
   end
 
   private
 
   def general_choosing(rule_name)
-    return if search_rules[rule_name].strip.empty?
+    return if str_cheker(search_rules[rule_name])
 
     @cars_list.keep_if { |car| car[rule_name].downcase == search_rules[rule_name].downcase }
   end
 
   def range_determining(rule_from, rule_to)
-    return if search_rules[rule_from].strip.empty? && search_rules[rule_to].strip.empty?
+    return if str_cheker(search_rules[rule_from]) && str_cheker(search_rules[rule_to])
 
-    if search_rules[rule_from].strip.empty?
+    if str_cheker(search_rules[rule_from])
       @cars_list.keep_if { |car| car['year'] <= search_rules[rule_to].to_i }
-    elsif search_rules[rule_to].strip.empty?
+    elsif str_cheker(search_rules[rule_to])
       @cars_list.keep_if { |car| car['year'] >= search_rules[rule_from].to_i }
     else
       @cars_list.keep_if { |car| (search_rules[rule_from].to_i..search_rules[rule_to].to_i).cover?(car['year']) }
     end
+  end
+
+  def str_cheker(str)
+    str.strip.empty?
+  end
+
+  def count_total_quantity
+    @total_quantity = cars_list.length
   end
 end
--- app/sorting_manager.rb
+++ app/sorting_manager.rb
@@ -3,6 +3,9 @@
 class SortingManager
   attr_accessor :results, :sort_option
 
+  DEFAULT_DIRECTION = 'asc'
+  DATE_FORMAT = '%d/%m/%Y'
+  
   def initialize(results, sort_option)
     @results = results
     @sort_option = sort_option
@@ -17,10 +20,10 @@ class SortingManager
   private
 
   def sort_by_parameter
-    sort_option['option(date_added|price)'] == 'price' ? results.sort_by! { |key| key['price'] } : results.sort_by! { |key| Date.strptime(key['date_added'], '%d/%m/%Y') }
+    sort_option['option(date_added|price)'] == 'price' ? results.sort_by! { |key| key['price'] } : results.sort_by! { |key| Date.strptime(key['date_added'], DATE_FORMAT) }
   end
 
   def sort_direction
-    results.reverse! unless sort_option['direction(desc|asc)'] == 'asc'
+    results.reverse! unless sort_option['direction(desc|asc)'] == DEFAULT_DIRECTION
   end
 end
