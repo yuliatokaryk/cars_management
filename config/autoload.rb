3a52b4bee947da8b48e65e2af3268eaa78fe2d1e
yuliatokaryk <julia4099@gmail.com>
2022-11-07 09:37:28 +0200

    i18n moved to i18n.rb

--- config/autoload.rb
+++ config/autoload.rb
@@ -11,6 +11,3 @@ require_relative '../app/cars_searcher'
 require_relative '../app/input_collector'
 require_relative '../app/output_manager'
 require_relative '../app/sorting_manager'
-
-I18n.load_path << Dir[File.expand_path("config/locales") + "/*.yml"]
-I18n.default_locale = :uk
