60c253dfcbc4f3d0481751cbc13dfd7a53c4e795
yuliatokaryk <julia4099@gmail.com>
2022-12-03 18:46:06 +0200

    added rules

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,6 +4,15 @@
 class CarsController < ApplicationController
   attr_accessor :params
 
+  INPUT_RULES = { 'make' => %w[required min_sym max_sym content],
+                  'model' => %w[required min_sym max_sym content],
+                  'year' => %w[required type max_value min_value],
+                  'odometer' => %w[required type min_value],
+                  'price' => %w[required type min_value],
+                  'description' => %w[required type max_value] }.freeze
+
+  CAR_RULES = %w[make model year odometer price description].freeze
+
   def create
     params['id'] = Time.now.to_i.to_s
     rules_collector
@@ -38,14 +47,71 @@ class CarsController < ApplicationController
   private
 
   def rules_collector
-    rules = %w[id make model year odometer price description date_added]
-    rules[1..-2].each do |rule|
+    CAR_RULES.each do |rule|
+      rule_message(rule)
       puts "please select #{rule}"
-      params[rule] = gets.chomp
+      value = gets.chomp
+      validator(rule, value) ? params[rule] = value : break
     end
   end
 
   def database
     Car.new
+  end
+
+  def rule_message(rule)
+    table = Terminal::Table.new title: rule.to_s.colorize(:yellow) do |t|
+      INPUT_RULES[rule].each do |el|
+        t << [I18n.t("add_rules.#{rule}.#{el}").colorize(:light_blue)]
+      end
+    end
+    puts table
+  end
+
+  def validator(rule, value)
+    case rule
+    when 'make' then make_validator(value)
+    when 'model' then model_validator(value)
+    when 'year' then year_validator(value)
+    when 'odometer' then odometer_validator(value)
+    when 'price' then price_validator(value)
+    when 'description' then description_validator(value)
+    end
+  end
+
+  def make_validator(make)
+    reg = /^[a-zA-Z]{3,50}$/
+    make.match?(reg)
+  end
+
+  def model_validator(model)
+    reg = /^[a-zA-Z]{3,50}$/
+    model.match?(reg)
+  end
+
+  def year_validator(year)
+    current_year = Time.new.year
+    min_year = 1900
+    return true if year.to_i >= min_year && year.to_i <= current_year
+
+    false
+  end
+
+  def odometer_validator(odometer)
+    return true if odometer.to_i >= 0
+
+    false
+  end
+
+  def price_validator(price)
+    return true if price.to_i >= 0
+
+    false
+  end
+
+  def description_validator(desc)
+    return true if desc.is_a?(String) && desc.length <= 5000 || desc == ''
+
+    false
   end
 end
