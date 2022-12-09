0011dd15ef5b1e7466a9198ef175606431deff9c
yuliatokaryk <julia4099@gmail.com>
2022-12-09 14:47:47 +0200

    removed useless condition

--- app/models/car.rb
+++ app/models/car.rb
@@ -16,7 +16,7 @@ class Car < ApplicationRecord
   def update(params)
     find_by('id', params['id'])
     params.each do |k, v|
-      @car[k] = v unless v.to_s.empty? || k == 'date_added'
+      @car[k] = v unless v.to_s.empty?
     end
     update_db(@cars)
   end
