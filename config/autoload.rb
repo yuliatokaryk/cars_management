9a5d33cc52e272e43e4829014ce81dd6c5de4669
yuliatokaryk <julia4099@gmail.com>
2022-12-17 19:16:39 +0200

    fixed autoload

--- config/autoload.rb
+++ config/autoload.rb
@@ -9,23 +9,21 @@ require 'bcrypt'
 require 'rake'
 require 'ffaker'
 require_relative 'locale_setter'
-require_relative '../app'
+require_relative '../lib/database'
+
+# controllers
 require_relative '../app/controllers/application_controller'
 require_relative '../app/controllers/cars_controller'
 require_relative '../app/controllers/session_controller'
 require_relative '../app/controllers/users_controller'
 require_relative '../app/controllers/users_searches_controller'
+require_relative '../app/controllers/menu_controller'
+
+# models
 require_relative '../app/models/application_record'
 require_relative '../app/models/car'
 require_relative '../app/models/user'
 require_relative '../app/models/user_searches'
-require_relative '../app/statistics_manager'
-require_relative '../app/cars_searcher'
-require_relative '../app/input_collector'
-require_relative '../app/main_menu'
-require_relative '../app/output_manager'
-require_relative '../app/sorting_manager'
-require_relative '../lib/database'
 
 # validations
 require_relative '../app/validations/cars_validator'
@@ -34,3 +32,14 @@ require_relative '../app/validations/email_validator'
 
 # views
 require_relative '../app/views/cars/index'
+require_relative '../app/views/menu/admin'
+require_relative '../app/views/menu/user'
+
+# services
+require_relative '../app/services/app_service'
+require_relative '../app/services/app'
+require_relative '../app/services/statistics_manager'
+require_relative '../app/services/cars_searcher'
+require_relative '../app/services/input_collector'
+require_relative '../app/services/output_manager'
+require_relative '../app/services/sorting_manager'
