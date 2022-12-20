d4ef2bfa9a15dc20f1bb26b496cdaf743d0086ea
yuliatokaryk <julia4099@gmail.com>
2022-12-20 12:07:01 +0200

    fixed menu locales

--- app/views/menu/user.rb
+++ app/views/menu/user.rb
@@ -12,7 +12,7 @@ class UserMenu
   def show_menu(hash)
     table = Terminal::Table.new title: I18n.t('menu.hint').to_s.colorize(:yellow) do |t|
       hash.each do |key, value|
-        t << [key, value.to_s.colorize(:blue)]
+        t << [I18n.t("menu_options.#{key}"), value.to_s.colorize(:blue)]
       end
       t.style = { all_separators: true }
     end
