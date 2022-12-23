4b387a27ee47ac0f095ce495425283c9c5436dbd
yuliatokaryk <julia4099@gmail.com>
2022-12-23 13:54:15 +0200

    added flash methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,19 @@ class ApplicationController
     @params = params
   end
 
-  def error_message(message)
-    puts I18n.t(message).colorize(:red)
-  end
-
-  def success_message(message)
-    puts I18n.t(message).colorize(:green)
-  end
-
   def flash
     @flash ||= Flash.new
+  end
+
+  def error(error_message)
+    flash.error([I18n.t("flash.error.#{error_message}")])
+  end
+
+  def message(flash_message)
+    flash.message([I18n.t("flash.message.#{flash_message}")])
+  end
+
+  def question(question_message)
+    flash.question(I18n.t("flash.question.#{question_message}"))
   end
 end
