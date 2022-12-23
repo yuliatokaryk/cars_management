97fe36c330bce8960f490e3961110c43658bcb6b
yuliatokaryk <julia4099@gmail.com>
2022-12-23 16:10:13 +0200

    cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,45 +4,41 @@
 class CarsController < ApplicationController
   attr_accessor :params
 
-  INPUT_RULES = { 'make' => %w[required min_sym max_sym content],
-                  'model' => %w[required min_sym max_sym content],
-                  'year' => %w[required type max_value min_value],
-                  'odometer' => %w[required type min_value],
-                  'price' => %w[required type min_value],
-                  'description' => %w[required type max_value] }.freeze
-
   CAR_RULES = %w[make model year odometer price description].freeze
 
-  def index
-    view.index(database.all)
+  def index(cars = database.all)
+    view.index(cars)
   end
 
   def show(user)
-    SearchManager.new(user, database.all).call
+    cars = SearchManager.new(user, database.all).call
+    index(cars)
   end
 
   def new
-    add_car_id
-    add_car_rules
-    add_current_data
-    create if params.length == 8
+    car = CarManager.new.create
+    create(car) if car
   end
 
   def edit
     id = target_id
-    return error('car_not_found') unless database.find_by('id', id)
+    car = database.find_by('id', id)
+    return error('car_not_found') unless car
 
-    params['id'] = id
-    edit_manager
+    view.index([car])
+    view.edit(CAR_RULES)
+
+    edited_car = CarManager.new(car).edit
+    update(edited_car)
   end
 
-  def create
-    database.create(params)
+  def create(car)
+    database.create(car)
     message('advert.create')
   end
 
-  def update
-    database.update(params)
+  def update(car)
+    database.update(car)
     message('advert.update')
   end
 
@@ -61,70 +57,11 @@ class CarsController < ApplicationController
     gets.chomp
   end
 
-  def add_car_id
-    params['id'] = Time.now.to_i.to_s
-  end
-
-  def add_current_data
-    params['date_added'] = Time.now.strftime('%d/%m/%Y')
-  end
-
-  def add_car_rules
-    CAR_RULES.each do |rule|
-      rule_message(rule)
-      question("car.edit.#{rule}")
-      value = gets.chomp
-      unless validator.call(rule, value)
-        puts error('invalid_car_rule')
-        break
-      end
-      save_value(rule, value)
-    end
-  end
-
-  def save_value(rule, value)
-    numeric_value = %w[year odometer price]
-    value = value.to_i if numeric_value.include? rule
-    params[rule] = value
-  end
-
-  def rule_message(rule)
-    rules = []
-    INPUT_RULES[rule].each do |el|
-      rules << I18n.t("flash.hint.advert_rules.#{rule}.#{el}")
-    end
-    flash.hint(I18n.t("flash.hint.advert_rules.#{rule}.title"), rules)
-  end
-
-  def edit_manager
-    view.edit(CAR_RULES)
-    edit_input
-  end
-
-  def edit_input
-    input = gets.chomp
-    return update if input == 'save'
-
-    input.to_i.between?(0, CAR_RULES.length - 1) ? edit_rule(input) : edit_manager
-  end
-
-  def edit_rule(index)
-    rule = CAR_RULES[index.to_i]
-    question('advert.edit_rule')
-    value = gets.chomp
-    save_value(rule, value) if validator.call(rule, value)
-    edit_manager
-  end
-
-  def database
-    @database ||= Car.new('cars')
-  end
-
   def view
     @view ||= Cars.new
   end
 
-  def validator
-    @validator ||= CarsValidator.new
+  def database
+    @database ||= Car.new('cars')
   end
 end
---
+++ app/services/car_manager.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+# service to create and edit car
+class CarManager
+  INPUT_RULES = { 'make' => %w[required min_sym max_sym content],
+                  'model' => %w[required min_sym max_sym content],
+                  'year' => %w[required type max_value min_value],
+                  'odometer' => %w[required type min_value],
+                  'price' => %w[required type min_value],
+                  'description' => %w[required type max_value] }.freeze
+
+  CAR_RULES = %w[make model year odometer price description].freeze
+
+  def initialize(car = {})
+    @car = car
+  end
+
+  def create
+    add_id
+    add_rules
+    add_current_data
+    @car.length == 8 ? @car : nil
+  end
+
+  def edit
+    flash.question(I18n.t('flash.question.car.edit'))
+    input = gets.chomp
+    return @car if input == 'save'
+
+    return edit_rule(input.to_i - 1) if input.to_i.between?(1, CAR_RULES.length)
+
+    edit
+  end
+
+  private
+
+  def add_id
+    @car['id'] = Time.now.to_i.to_s
+  end
+
+  def add_rules
+    CAR_RULES.each do |rule|
+      rule_message(rule)
+      flash.question(I18n.t("flash.question.car.create.#{rule}"))
+      value = gets.chomp
+      unless validator.call(rule, value)
+        puts flash.error([I18n.t('flash.error.invalid_car_rule')])
+        break
+      end
+      save_value(rule, value)
+    end
+  end
+
+  def add_current_data
+    @car['date_added'] = Time.now.strftime('%d/%m/%Y')
+  end
+
+  def edit_rule(index)
+    rule = CAR_RULES[index]
+    flash.question(I18n.t('flash.question.advert.edit_rule'))
+    value = gets.chomp
+    save_value(rule, value) if validator.call(rule, value)
+    edit
+  end
+
+  def save_value(rule, value)
+    numeric_value = %w[year odometer price]
+    value = value.to_i if numeric_value.include? rule
+    @car[rule] = value
+  end
+
+  def rule_message(rule)
+    rules = []
+    INPUT_RULES[rule].each do |el|
+      rules << I18n.t("flash.hint.advert_rules.#{rule}.#{el}")
+    end
+    flash.hint(I18n.t("flash.hint.advert_rules.#{rule}.title"), rules)
+  end
+
+  def validator
+    @validator ||= CarsValidator.new
+  end
+
+  def flash
+    @flash ||= Flash.new
+  end
+end
