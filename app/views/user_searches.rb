eeba259253174904d730951efa187dc3a82eb932
yuliatokaryk <julia4099@gmail.com>
2022-12-23 13:56:00 +0200

    user searches view

--- app/views/user_searches.rb
+++ app/views/user_searches.rb
@@ -3,7 +3,6 @@
 # service to generate statistics show view
 class UserSearches
   def show(results)
-    puts I18n.t('user_searches.searches_title').colorize(:green)
     results.each do |result|
       show_day_result(result)
     end
@@ -13,7 +12,7 @@ class UserSearches
     table = Terminal::Table.new title: search_result[0]['date'].colorize(:yellow) do |t|
       search_result[1..].each do |search|
         row = []
-        search.each { |key, value| row << "#{key} : #{value}" }
+        search.each { |key, value| row << "#{I18n.t("cars.cars_params.#{key}")}: #{value}" }
         t << row
       end
       t.style = { all_separators: true }
